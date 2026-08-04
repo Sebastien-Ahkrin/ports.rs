453547efbe2a66c24a15e2ce731dcf0217f94b5a
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2026-08-04 20:03:10 +0200

    feat: add name to port

---
+++ src/database.rs
@@ -0,0 +1,87 @@
+use std::collections::HashMap;
+use std::str::FromStr;
+
+const NMAP_SERVICES: &str = include_str!("nmap-services.txt");
+
+#[derive(PartialEq, Eq, Hash, Copy, Clone, Debug)]
+pub enum Protocol {
+    Tcp,
+    Udp,
+    Sctp
+}
+
+#[derive(Debug)]
+pub struct Service {
+    pub name: String,
+    pub port: u16,
+    pub protocol: Protocol,
+}
+
+pub struct Database {
+    /// (port, Tcp/Udp) -> Service
+    services: HashMap<(u16, Protocol), Service>,
+}
+
+fn parse_port_protocol(data: &str) -> (u16, Protocol) {
+    let mut split = data.split("/");
+
+    let port = split.next().expect("cannot parse port");
+    let protocol = split.next().expect("cannot parse protocol");
+
+    (u16::from_str(port).expect("cannot get u16 from str"), match protocol {
+        "tcp" => Protocol::Tcp,
+        "udp" => Protocol::Udp,
+        "sctp" => Protocol::Sctp,
+        _ => panic!("unknown protocol: {:?} (data = {:?})", protocol, data)
+    })
+}
+
+impl Default for Database {
+    fn default() -> Self {
+        let mut services = HashMap::new();
+
+        for line in NMAP_SERVICES.lines() {
+            let line = line.trim();
+
+            // remove header (commentary)
+            if line.starts_with('#') {
+                continue;
+            }
+
+            // cspmlockmgr	1272/tcp	0.000380 => [cspmlockmgr, 1272/tcp, 0.000380
+            let mut parts = line.split_whitespace();
+
+            let Some(name) = parts.next() else { continue };
+            let Some(port_protocol) = parts.next() else { continue };
+
+            let (port, protocol) = parse_port_protocol(&port_protocol);
+
+            services.insert((port, protocol), Service { name: name.to_string(), port, protocol });
+        }
+
+        Self { services }
+    }
+}
+
+impl Database {
+    pub fn lookup(&self, port: u16, protocol: Protocol) -> Option<&Service> {
+        self.services.get(&(port, protocol))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // mysql	3306/tcp	0.045390
+    #[test]
+    fn get_mysql_from_database() {
+        let database = Database::default();
+        let service = database.lookup(3306, Protocol::Tcp);
+
+        assert!(service.is_some());
+        assert_eq!(service.unwrap().name, "mysql");
+        assert_eq!(service.unwrap().port, 3306);
+        assert_eq!(service.unwrap().protocol, Protocol::Tcp);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,24 @@
+mod database;
+
 use std::net::TcpStream;
+use crate::database::{Database, Protocol};
 
 /// Check if a specific port is already open
 pub fn is_port_open(port: u16) -> bool {
     let connection = TcpStream::connect(("127.0.0.1", port));
     connection.is_ok()
+}
+
+/// Check if a specific port is already open and give the name too
+pub fn is_port_open_with_name(port: u16) -> (bool, String) {
+    let database = Database::default();
+    let result = is_port_open(port);
+    
+    let service = database.lookup(port, Protocol::Tcp);
+    assert!(service.is_some());
+    
+    let name = &service.unwrap().name;
+    (result, name.clone())
 }
 
 /// Check if range of ports is allocated. Return a Vec<u16> with ports that is allocated
@@ -17,6 +32,21 @@ pub fn ports_open_in_range(ports: Vec<u16>) -> Vec<u16> {
         }
     }
 
+    allocated_ports
+}
+
+/// Check if range of ports is allocated. Return a Vec<(bool, u16, String)> with (port, name) that is allocated
+pub fn ports_open_in_range_with_name(ports: Vec<u16>) -> Vec<(u16, String)> {
+    let mut allocated_ports = Vec::new();
+    
+    for port in ports {
+        let result = is_port_open_with_name(port);
+        match result {
+            (true, name) => allocated_ports.push((port, name)),
+            (false, name) => continue,
+        }
+    }
+    
     allocated_ports
 }
 
